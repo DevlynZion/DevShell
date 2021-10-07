7aa8357cc0eb35af4d20230ec0ffe2f2f8434583
DevlynZioN <zionn.man@gmail.com>
2021-10-07 11:55:02 +0200

    Add ListMenu

---
+++ DevShell/DevShell.Common/Extend/ExtendConsole.cs
@@ -0,0 +1,25 @@
+﻿using DevShell.Common.ConsoleUI;
+using DevShell.Common.Output;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FooConsole = System.Console;
+
+namespace DevShell.Common.Extend
+{
+    public static class Console
+    {
+        public static void ListMenu(string description, IEnumerable<ConsoleMenuItem> menuItems)
+        {
+            ConsoleHelper.ListMenu(description, menuItems);
+        }
+
+        public static int ListMenuParams(string description, params string[] options)
+        {
+            return ConsoleHelper.ListMenuParams(description, options);
+        }
+    }
+}
---
+++ DevShell/DevShell.Common/Output/ConsoleHelper.cs
@@ -0,0 +1,33 @@
+﻿using DevShell.Common.ConsoleUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevShell.Common.Output
+{
+    public class ConsoleHelper
+    {
+        public static void ListMenu(string description, IEnumerable<ConsoleMenuItem> menuItems)
+        {
+            var menu = new ConsoleMenu<string>(description, menuItems.ToArray());
+            menu.RunConsoleMenu();
+        }
+
+        private static string itemSelected;
+        public static int ListMenuParams(string description, params string[] options)
+        {
+            var menuItems = options.Select(o => new ConsoleMenuItem<string>(o, itemCallback, o));
+
+            ListMenu(description, menuItems);
+
+            return options.ToList().IndexOf(itemSelected);
+        }
+
+        private static void itemCallback(string itemClicked)
+        {
+            itemSelected = itemClicked;
+        }
+    }
+}
