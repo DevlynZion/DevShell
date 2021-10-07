099932f0efa4f63454de9126b17c1da8eb1ecd47
DevlynZion <DevlynZion@users.noreply.github.com>
2021-10-07 13:57:10 +0200

    New Outputs

---
+++ DevShell/DevShell.Cmdlets/Output/WriteHeader.cs
@@ -0,0 +1,28 @@
+﻿using DevShell.Common.Output;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevShell.Cmdlets.Output
+{
+    [Cmdlet(VerbsCommunications.Write, "Header")]
+    public class WriteHeader : Cmdlet
+    {
+        [Parameter(Position = 0)]
+        public string Text { get; set; } = "";
+        [Parameter(Position = 1)]
+        public string Subtitle { get; set; } = "";
+        [Parameter(Position = 2)]
+        public string ForeGroundColor { get; set; } = "White";
+
+        protected override void ProcessRecord()
+        {
+            var foreGroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), ForeGroundColor);
+
+            ConsoleHelper.Header(Text, Subtitle, foreGroundColor);
+        }
+    }
+}
