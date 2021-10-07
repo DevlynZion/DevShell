34201907db7ff73b18cb665f14b4fd440da2f591
DevlynZion <DevlynZion@users.noreply.github.com>
2021-10-07 08:09:23 +0200

    Add Icon

--- DevShell/DevShell.Console/Script/Alias.ps1
+++ DevShell/DevShell.Console/Script/Alias.ps1
@@ -2,6 +2,4 @@
 Write-Host "Alias"
 Write-Host "-----"
 
-
-DevShell-Add-Alias "MergeTFStoDevOps" "C:\Users\dvanderwalt\OneDrive - MineRP\Desktop\MergeMainMaster.bat"
-DevShell-Add-Alias "np" "C:\Program Files\Notepad++\notepad++.exe"
+#DevShell-Add-Alias "np" "C:\Program Files\Notepad++\notepad++.exe"
