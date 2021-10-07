0bb3e605c7d44593557fdb60504f220a9d81eb65
DevlynZioN <zionn.man@gmail.com>
2021-10-07 09:08:18 +0200

    Add Work Code

---
+++ DevShell/DevShell.Cmdlets/CADEnviroment/ClearCoreModules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevShell.Cmdlets.CADEnviroment
+{
+    [Cmdlet(VerbsCommon.Clear, "CoreModules")]
+    public class ClearCoreModules : Cmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string Environment { get; set; } 
+        protected override void ProcessRecord() 
+        {
+            var path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), @"MineRP\CAFModules");
+
+            DirectoryInfo di = new DirectoryInfo(Path.Combine(path, Environment));
+
+            di.Delete(true);
+        }
+    }
+}
