9cff742df1cff035290fc471c44ebfd31db6f178
DevlynZioN <zionn.man@gmail.com>
2021-10-12 15:40:21 +0200

    Update ConsoleTable.cs

--- DevShell/DevShell.Common/ConsoleUI/ConsoleTable.cs
+++ DevShell/DevShell.Common/ConsoleUI/ConsoleTable.cs
@@ -21,43 +21,48 @@ namespace DevShell.Common.ConsoleUI
             var columns = Data.Columns;
             var columnsLength = new List<int>();
 
-            foreach(DataColumn dataColumn in columns)
+            foreach (DataColumn dataColumn in columns)
             {
                 int colLength = dataColumn.ColumnName.Length + 1;
 
-                foreach(DataRow dataRow in Data.Rows)
+                foreach (DataRow dataRow in Data.Rows)
                 {
                     var dataLength = dataRow[dataColumn].ToString().Length + 1;
 
-                    if(dataLength > colLength)
+                    if (dataLength > colLength)
                         colLength = dataLength;
                 }
 
                 columnsLength.Add(colLength);
             }
 
-            if (Console.WindowWidth < columnsLength.Sum())
+            var columnTotal = columnsLength.Sum();
+
+            if (Console.WindowWidth < columnTotal)
             {
                 Console.WriteLine("Can't fit table into console.");
                 return;
             }
 
 
+            var spaceLeft = Console.WindowWidth - columnTotal;
+            var spaceToAdd = spaceLeft / columns.Count;
+
             string headings = "";
             string breakLine = "";
-            for(int k = 0; k < columns.Count;  k++)
+            for (int k = 0; k < columns.Count; k++)
             {
                 DataColumn dataColumn = columns[k];
                 var maxLength = columnsLength[k];
-                var space = new string(' ', maxLength - dataColumn.ColumnName.Length);
+                var space = new string(' ', maxLength - dataColumn.ColumnName.Length + spaceToAdd);
 
                 headings += string.Format("{0}{1}", dataColumn.ColumnName, space);
-                breakLine += new string('-', maxLength - 1) + " ";
+                breakLine += new string('-', maxLength - 1) + new string(' ', 1 + spaceToAdd);
             }
             Console.WriteLine(headings);
             Console.WriteLine(breakLine);
 
-            foreach(DataRow dataRow in Data.Rows)
+            foreach (DataRow dataRow in Data.Rows)
             {
                 var rowLine = "";
                 for (int k = 0; k < columns.Count; k++)
@@ -65,7 +70,7 @@ namespace DevShell.Common.ConsoleUI
                     DataColumn dataColumn = columns[k];
                     var maxLength = columnsLength[k];
                     var value = dataRow[dataColumn].ToString();
-                    var endSpace = new string(' ', maxLength - value.Length);
+                    var endSpace = new string(' ', maxLength - value.Length + spaceToAdd);
 
                     rowLine += value + endSpace;
                 }
