18d47e5e47644629ab50f9817f401037d32480da
DevlynZioN <zionn.man@gmail.com>
2021-10-12 15:27:07 +0200

    Add DataTable display tool

---
+++ DevShell/Cmdlets/DevShell.Cmdlets/Test/TestTable.cs
@@ -0,0 +1,46 @@
+﻿using DevShell.Common.ConsoleUI;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevShell.Cmdlets.Test
+{
+    [Cmdlet(VerbsDiagnostic.Test, "Table")]
+    public class TestTable : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("string", typeof(string));
+            dt.Columns.Add("int", typeof(int));
+            dt.Columns.Add("double", typeof(double));
+            dt.Columns.Add("float", typeof(float));
+
+            NewRow(dt, "Row 1", 2334, 232332.222, 521156.23f);
+            NewRow(dt, "Row 2", 6335, 3.222, 552226.23211f);
+            NewRow(dt, "Row 3", 3, 3.2233332, 53356.2223f);
+            NewRow(dt, "Row 4", 44556565, 44.23322, 53356.2223f);
+            NewRow(dt, "Row 5", 234343434, 232.24422, 51156.23333f);
+
+
+            ConsoleTable table = new ConsoleTable(dt);
+
+            table.Draw();
+        }
+
+        private void NewRow(DataTable dt, string c1, int c2, double c3, float c4)
+        {
+            var r1 = dt.NewRow();
+            r1[0] = c1;
+            r1[1] = c2;
+            r1[2] = c3;
+            r1[3] = c4;
+            dt.Rows.Add(r1);
+        }
+    }    
+}
---
+++ DevShell/DevShell.Common/ConsoleUI/ConsoleTable.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevShell.Common.ConsoleUI
+{
+    public class ConsoleTable
+    {
+        public DataTable Data { get; set; }
+
+        public ConsoleTable(DataTable data)
+        {
+            Data = data;
+        }
+
+        public void Draw()
+        {
+            var columns = Data.Columns;
+            var columnsLength = new List<int>();
+
+            foreach(DataColumn dataColumn in columns)
+            {
+                int colLength = dataColumn.ColumnName.Length + 1;
+
+                foreach(DataRow dataRow in Data.Rows)
+                {
+                    var dataLength = dataRow[dataColumn].ToString().Length + 1;
+
+                    if(dataLength > colLength)
+                        colLength = dataLength;
+                }
+
+                columnsLength.Add(colLength);
+            }
+
+            if (Console.WindowWidth < columnsLength.Sum())
+            {
+                Console.WriteLine("Can't fit table into console.");
+                return;
+            }
+
+
+            string headings = "";
+            string breakLine = "";
+            for(int k = 0; k < columns.Count;  k++)
+            {
+                DataColumn dataColumn = columns[k];
+                var maxLength = columnsLength[k];
+                var space = new string(' ', maxLength - dataColumn.ColumnName.Length);
+
+                headings += string.Format("{0}{1}", dataColumn.ColumnName, space);
+                breakLine += new string('-', maxLength - 1) + " ";
+            }
+            Console.WriteLine(headings);
+            Console.WriteLine(breakLine);
+
+            foreach(DataRow dataRow in Data.Rows)
+            {
+                var rowLine = "";
+                for (int k = 0; k < columns.Count; k++)
+                {
+                    DataColumn dataColumn = columns[k];
+                    var maxLength = columnsLength[k];
+                    var value = dataRow[dataColumn].ToString();
+                    var endSpace = new string(' ', maxLength - value.Length);
+
+                    rowLine += value + endSpace;
+                }
+                Console.WriteLine(rowLine);
+            }
+        }
+    }
+}
