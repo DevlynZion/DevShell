3b9f044bf8a8b02254441d608e95ce28a08d6769
DevlynZioN <zionn.man@gmail.com>
2021-11-25 14:21:06 +0200

    ImpersonatedFileCopy

---
+++ DevShell/DevShell.Common/Utility/ImpersonatedFileCopy.cs
@@ -0,0 +1,120 @@
+﻿using System.Security.Principal;
+using System.Security.Permissions;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+using System;
+
+namespace DevShell.Common.Utility
+{
+    // https://coderedirect.com/questions/99769/copy-file-on-a-network-shared-drive
+    public class ImpersonatedFileCopy : IDisposable
+    {
+        #region Assembly Functions
+        [DllImport("advapi32.dll")]
+        public static extern bool LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr handle);
+        #endregion
+
+        #region Private Variables
+        private IntPtr _TokenHandle = new IntPtr(0);
+        private WindowsImpersonationContext _WindowsImpersonationContext;
+        #endregion
+
+        #region Constructors
+        public ImpersonatedFileCopy(string domain, string username, string password)
+        {
+            Impersonate(domain, username, password);
+        }
+        #endregion
+
+        #region Methods
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        private void Impersonate(string domain, string username, string password)
+        {
+            bool returnValue;
+
+            try
+            {
+                const int LOGON32_PROVIDER_DEFAULT = 0;
+                const int LOGON32_LOGON_INTERACTIVE = 2;
+
+                _TokenHandle = IntPtr.Zero;
+
+                //Call LogonUser to obtain a handle to an access token.
+                returnValue = LogonUser(username, domain, password, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, ref _TokenHandle);
+                if (returnValue)
+                {
+                    WindowsIdentity newId = new WindowsIdentity(_TokenHandle);
+                    _WindowsImpersonationContext = newId.Impersonate();
+                }
+            }
+            catch (Exception ex)
+            {
+                UndoImpersonate();
+                var fc = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error" + ex.Message);
+                Console.ForegroundColor = fc;
+            }
+        }
+
+        private void UndoImpersonate()
+        {
+            if (_WindowsImpersonationContext != null)
+            {
+                _WindowsImpersonationContext.Undo();
+                if (!_TokenHandle.Equals(IntPtr.Zero))
+                {
+                    CloseHandle(_TokenHandle);
+                }
+            }
+        }
+
+        public bool PutFile(FileStream source, string destRemoteFilename, bool overwrite)
+        {
+            try
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(destRemoteFilename))) Directory.CreateDirectory(Path.GetDirectoryName(destRemoteFilename));
+                using (FileStream dest = File.OpenWrite(destRemoteFilename))
+                {
+                    source.Seek(0, SeekOrigin.Begin);
+                    source.CopyTo(dest);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool GetFile(string sourceRemoteFilename, FileStream dest, bool overwrite)
+        {
+            try
+            {
+                using (FileStream source = File.OpenRead(sourceRemoteFilename))
+                {
+                    source.Seek(0, SeekOrigin.Begin);
+                    source.CopyTo(dest);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region IDisposable
+        public void Dispose()
+        {
+            UndoImpersonate();
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
